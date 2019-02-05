6b778c5bdb720b3b629049072cbcde46eb6c075c
github username <github email address>
2019-02-04 21:38:58 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :authors
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
+  resources :authors 
 end
